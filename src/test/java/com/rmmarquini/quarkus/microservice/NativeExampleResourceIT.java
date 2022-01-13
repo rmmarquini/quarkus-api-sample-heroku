4a0d198b079f7f3866e93f116cad7d95d7060cf9
Rafa Marquini <rafa.mardegan@gmail.com>
2022-01-13 11:36:28 -0300

    Initial commit - API done

---
+++ src/test/java/com/rmmarquini/quarkus/microservice/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.rmmarquini.quarkus.microservice;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
